56f7265487a56a297d9d6c2a4a71268fcd5cc945
Ibrangrd <ibrangrd@gmail.com>
2024-07-10 20:22:18 +0530

    Fix the problem

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const items = document.querySelectorAll('.image-item-wrapper');
+const addBtn = document.querySelector(".add");
+const input = document.querySelector(".inp-group");
 
-    items.forEach(function (item) {
-        const link = item.querySelector('a');
-        const image = link.querySelector('img');
-        const videoSrc = link.getAttribute('href').replace('html', 'mp4'); // Assumes your videos are in mp4 format with the same name
 
-        // Create a video element
-        const video = document.createElement('video');
-        video.src = videoSrc;
-        video.className = 'preview-video';
-        video.muted = true;
-        video.loop = true;
-        video.style.display = 'none';
-        video.style.width = '100%';
-        video.style.height = '100%';
+function removeInput(){
+    this.parentElement.remove();
+}
 
-        // Insert the video element before the image
-        link.insertBefore(video, image);
+function addInput(){
+    const name = document.createElement("input");
+    name.type="text";
+    name.placeholder = "Enter Reciver's Name";
 
-        link.addEventListener('mouseenter', function () {
-            image.style.display = 'none';  // Hide the image
-            video.style.display = 'block'; // Show the video
-            video.play();                  // Play the video
-        });
+    const address = document.createElement("input");
+    address.type="text";
+    address.placeholder = "Enter Full Address ";
 
-        link.addEventListener('mouseleave', function () {
-            video.pause();                 // Pause the video
-            video.style.display = 'none';  // Hide the video
-            video.currentTime = 0;         // Reset the video to the beginning
-            image.style.display = 'block'; // Show the image
-        });
-    });
-});
+
+    const btn=document.createElement("a");
+    btn.className = "delete";
+    btn.innerHTML = "&times";
+
+    btn.addEventListener("click", removeInput);
+
+    const flex=document.createElement("div");
+    flex.className = "flex";
+    
+    input.appendChild(flex);
+    flex.appendChild(name);
+    flex.appendChild(address);
+    flex.appendChild(btn);
+}
+addBtn.addEventListener("click", addInput);
